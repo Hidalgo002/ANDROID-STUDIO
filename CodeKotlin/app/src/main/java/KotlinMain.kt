658fb8d7acbc1d121fd1d8b40aa1145ed49025e7
Hidalgo002 <vhidalgodominguez@gmail.com>
2025-10-03 14:29:27 +0200

    PSP

--- CodeKotlin/app/src/main/java/KotlinMain.kt
+++ CodeKotlin/app/src/main/java/KotlinMain.kt
@@ -1,4 +1,19 @@
 
 fun main(){
-    println("Hola Mundo")
+    val numPlanetasSistemaSolar : Int = 0
+    var planeta : String
+    planeta = "Tierra"
+    println("El numero de planetas del Sistema Solar es: "+numPlanetasSistemaSolar)
+    planeta = "Marte"
+    println("El planeta de los murcianos es: "+planeta)
+
+    //Byte
+    //Short
+    //Int
+    //Long
+    //Float
+    //Double
+    //Char = ' '
+    //String = " "
+    //boolean = true o false
 }
---
+++ CodeKotlin/app/src/main/java/com/example/codekotlin/Persona.kt
@@ -0,0 +1,13 @@
+package com.example.codekotlin
+
+class Persona (nombre:String, primerApellido:String, edad:Int){
+    var nombre = nombre
+    var primerApellido = primerApellido
+    var edad = edad
+
+    fun main(){
+        val persona1 = Persona("michel", "Fassbender",56)
+        print(persona1)
+        print(persona1.edad>50)
+    }
+}
