5b06f0d75757f17918b4a40fe2beac8b3746c2d5
Hidalgo002 <vhidalgodominguez@gmail.com>
2025-10-10 14:27:28 +0200

    10Oct

---
+++ CodeKotlin/app/src/main/java/reapaso/kotlin/Profesor.kt
@@ -0,0 +1,16 @@
+package reapaso.kotlin
+
+class Profesor constructor(nombre:String, edad:Int) {
+    var nombre: String = nombre
+    var edad: Int = edad
+
+
+
+    override fun toString (): String {
+        return "Nombre: " + nombre + "| Edad: " + edad
+    }
+
+    fun imprimirDatosProfe() {
+        println(toString())
+    }
+}
--- CodeKotlin/app/src/main/java/reapaso/kotlin/Repaso.kt
+++ CodeKotlin/app/src/main/java/reapaso/kotlin/Repaso.kt
@@ -1,5 +1,7 @@
 package reapaso.kotlin
 
+import kotlin.math.cos
+
 fun main(args: Array<String>){
     /*var numero:Int = 0 //variable de clase int
 
@@ -23,7 +25,7 @@ fun main(args: Array<String>){
     for (i in 1..10 step 2){
         //incrementamos de 2 en 2
         println(i)
-    }*/
+    }
 
 
 
@@ -53,5 +55,64 @@ fun main(args: Array<String>){
 
 
     }
+
+    //RECTÁNGULO
+    fun areaRectangulo(alto:Int, ancho:Int): Int {
+        return alto * ancho
+    }
+
+        print("Altura: ")
+        var alt = readln().toInt()
+        print("Ancho: ")
+        var anch = readln().toInt()
+        println("Area del cuadrado: ${areaRectangulo(alt, anch)} ")
+
+    //CUADRADO
+    fun areaCuadrado(lado:Int) = lado * lado
+
+        print("Lado: ")
+        var lado2 = readln().toInt()
+        print("Area del cuadrado:${areaCuadrado(lado2)}")
+
+
+
+
+
+    //QUE APAREZCAN DATOS POR DEFECTO
+    fun datosCurso(centro:String, titulacion:String="DAM"){
+        println("Centro: "+centro+" --> Titulacion: "+titulacion)
+    }
+    datosCurso("Santo Domingo savio")
+    datosCurso("Valle Inclán", "Administracion y Finanazas")
+
+
+    //KOTLIN ORDERNA DIRECTAMENTE LOS PARÁMETROS SI SE RESPETA EL NOMBRE, es decir, si no recordamos
+    // el orden de los parámetros pero si los nombres, podemos escribirlos y se ordenaran autmaticamete en el constructor
+    fun costeKilometro(matricula:String, costeKilomtro: Double, kilometro:Double){
+        var costeTotal = costeKilomtro * kilometro
+        println("El coste total del coche ["+matricula+"] -> "+costeTotal)
+    }
+    costeKilometro("9875 GFT",0.148,265.8)
+    costeKilometro(costeKilomtro = 0.15, kilometro = 89.8, matricula = "8756 PLA")
+    costeKilometro( kilometro= 0.85,  costeKilomtro= 145.0, matricula = "3467 PLA")
+
+
+    var alumnos: IntArray
+    alumnos = IntArray(5)
+
+    println("Mete la nota del alumno: ")
+    for (i in 0..4){
+        println("Nota["+i+"]: ")
+        alumnos[i] = readln().toInt()
+    }
+
+    println("-----NOTAS-----")
+    for (i in 0..4){
+        println(alumnos[i])
+    }
+*/
+
+    val profesor1 = Profesor("James", 42)
+    profesor1.imprimirDatosProfe()
 
 }
